aa6af241fc18135b2db0e47758bc10eed84eb360
dansantander <dansantandr@gmail.com>
2020-12-30 14:48:55 -0600

    feat: put route completed

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -29,7 +29,27 @@ router.delete('/:id', (req, res) => {
       movies.splice(i, 1);
     }
   });
-  res.json(movies)
+  res.json(movies);
+})
+
+// update
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const { title, director, year, rating } = req.body;
+  console.log('title', title);
+  if (title && director && year && rating) {
+    _.each(movies, (movie, i) => {
+      if (movie.id == id) {
+        movie.title = title;
+        movie.director = director;
+        movie.year = year;
+        movie.rating = rating;
+      }
+    });
+    res.json(movies);
+  } else {
+    res.status(500).json({error: 'Missing params'});
+  }
 })
 
 module.exports = router;
