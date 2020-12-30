933ead3c5d45c00a9f576e3c5e46858d9f81371d
dansantander <dansantandr@gmail.com>
2020-12-30 15:01:38 -0600

    feat: fetching users from jsonplaceholder

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 // routes
 app.use(require('./routes/index'));
 app.use('/api/movies', require('./routes/movies'));
+app.use('/api/users', require('./routes/users'));
 
 // starting server
 app.listen(app.get('port'), () => {
